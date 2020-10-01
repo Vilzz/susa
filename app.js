ccd870951fa1e844b76f205a43c261948f5709d5
Vilzz <n79613928275@gmail.com>
2020-10-01 11:46:07 +0400

    Custom error handler added

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const App = express();
+const app = express();
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 const PORT = process.env.PORT || 5000;
-const server = App.listen(PORT, () =>
+
+app.get("/", (req, res) => {
+  res.send("<h1>Работает</h1>");
+});
+
+const server = app.listen(PORT, () =>
   console.log(
     `Сервер запущен в режиме ${process.env.NODE_ENV} на порту ${PORT}`
   )
---
+++ middleware/errorHandler.js
@@ -0,0 +1,25 @@
+const ErrorResponse = require("../utils/errorResponse");
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+  if (err.name === "CastError") {
+    const message = "Ресурс не найден";
+    error = new ErrorResponse(message, 404);
+  }
+  if (err.code === 11000) {
+    const message = `Поле базы данных уже содержит такую запись -'${Object.keys(
+      err.keyValue
+    )}':'${Object.values(err.keyValue)}'`;
+    error = new ErrorResponse(message, 404);
+  }
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Ошибка на стороне сервера",
+  });
+};
+
+module.exports = errorHandler;
